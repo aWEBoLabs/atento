008a978ca828a47fcafdf9bc5b04cc28014e7cb4
cristian100 <cristian100@gmail.com>
2015-08-31 22:49:57 -0600

    added content area for the landing page

--- sites/all/themes/atento/templates/page--taxonomy--vid-1.tpl.php
+++ sites/all/themes/atento/templates/page--taxonomy--vid-1.tpl.php
@@ -104,11 +104,6 @@
         <div class="l-content-inner-inner">
           <?php print $breadcrumb; ?>
           <a id="main-content"></a>
-          <?php print render($title_prefix); ?>
-          <?php if ($title): ?>
-            <h1><?php print $title; ?></h1>
-          <?php endif; ?>
-          <?php print render($title_suffix); ?>
           <?php print $messages; ?>
           <?php print render($tabs); ?>
           <?php print render($page['help']); ?>
@@ -116,7 +111,7 @@
             <ul class="action-links"><?php print render($action_links); ?></ul>
           <?php endif; ?>
           <?php print render($page['content']); ?>
-          <?php print $feed_icons; ?>
+          <?php //print $feed_icons; ?>
         </div>
       </div>
     </div>
