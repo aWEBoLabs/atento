58401cf7e9f1387f6ef064cc4b7eac2118a54f63
cristian100 <cristian100@gmail.com>
2015-09-27 16:51:59 -0600

    finished login

---
+++ sites/all/modules/custom/atentom/templates/atentom-user_login-markup-footer.tpl.php
@@ -0,0 +1,1 @@
+<p class="login-footer">* Access, use and exploitation of all content in this site is intended for current Atento employees only. Access credentials are confidential and non transferrable and violations may lead to administrative and legal sanctions.</p>
---
+++ sites/all/modules/custom/atentom/templates/atentom-user_login-markup.tpl.php
@@ -0,0 +1,2 @@
+<h1>Brand Room Access</h1>
+<p>Please enter your username and password</p>
