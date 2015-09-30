7aca24358a75eafedf084fc4852f58214a802299
cristian100 <cristian100@gmail.com>
2015-09-29 18:33:26 -0600

    added new script for managing the background

--- sites/all/themes/atento/js/atento.logged-out.bg.js
+++ sites/all/themes/atento/js/atento.logged-out.bg.js
@@ -6,7 +6,16 @@
   Drupal.behaviors.atentoBG = {
     attach: function (context, settings) {
       $('body.not-logged-in', context).once('atento-bg', function () {
-        console.dir(settings.atento.bg_images);
+        var image = settings.atento.bg_images[Math.floor(Math.random()*settings.atento.bg_images.length)];
+        $('body').css({
+          'background': 'url(' + image + ') no-repeat center center fixed', 
+          '-webkit-background-size': 'cover', 
+          '-moz-background-size': 'cover', 
+          '-o-background-size': 'cover', 
+          'background-size': 'cover', 
+          'filter': 'progid:DXImageTransform.Microsoft.AlphaImageLoader(src=\'.myBackground.jpg\', sizingMethod=\'scale\')', 
+          '-ms-filter': "progid:DXImageTransform.Microsoft.AlphaImageLoader(src='myBackground.jpg', sizingMethod='scale')"
+        });
       });
     }
   };
