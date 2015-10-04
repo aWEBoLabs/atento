5bd2ea24cf194e434ac3da95dce2ed4498e8a938
cristian100 <cristian100@gmail.com>
2015-10-03 23:44:35 -0600

    added new styling for files and accordions

--- sites/all/themes/atento/js/atento.behaviors.js
+++ sites/all/themes/atento/js/atento.behaviors.js
@@ -16,27 +16,33 @@
   };
 
   /**
-   * Behaviors are Drupal's way of applying JavaScript to a page. In short, the
-   * advantage of Behaviors over a simple 'document.ready()' lies in how it
-   * interacts with content loaded through Ajax. Opposed to the
-   * 'document.ready()' event which is only fired once when the page is
-   * initially loaded, behaviors get re-executed whenever something is added to
-   * the page through Ajax.
-   *
-   * You can attach as many behaviors as you wish. In fact, instead of overloading
-   * a single behavior with multiple, completely unrelated tasks you should create
-   * a separate behavior for every separate task.
-   *
-   * In most cases, there is no good reason to NOT wrap your JavaScript code in a
-   * behavior.
-   *
-   * @param context
-   *   The context for which the behavior is being executed. This is either the
-   *   full page or a piece of HTML that was just added through Ajax.
-   * @param settings
-   *   An array of settings (added through drupal_add_js()). Instead of accessing
-   *   Drupal.settings directly you should use this because of potential
-   *   modifications made by the Ajax callback that also produced 'context'.
+   * Behavior for Files Rows
+   */
+  Drupal.behaviors.atentoFilesRows = {
+    attach: function (context, settings) {
+      $('.files.photoswipe-gallery', context).once('filesRows', function () {
+        // Open First by Default
+        var items = $(this).find('.file-preview');
+        var count = items.length;
+        var rows = Math.ceil(count/3);
+        for(i=0; i<rows; i++) {
+          start = i*3;
+          end = (i+1)*3;
+          chunk = $(items).slice(start, end);
+          $(chunk).addClass('row-' + (i+1));
+          if ( i == 0 ) {
+            $(chunk).addClass('row-first');
+          }
+          if ( i == (rows-1) ) {
+            $(chunk).addClass('row-last');
+          }
+        }
+      });
+    }
+  };
+  
+  /**
+   * Behavior for Accordion
    */
   Drupal.behaviors.atentoAccordion = {
     attach: function (context, settings) {
@@ -46,6 +52,7 @@
         
         // Add Click Event
         $(this).find('.accordion-item .field-name-field-title').click(atentoAccordionClick);
+        
       });
     }
   };
