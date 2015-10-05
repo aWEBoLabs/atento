d38d8664435ddf1af8ca9bfdcebce88e875b4b8d
cristian100 <cristian100@gmail.com>
2015-10-04 21:25:26 -0600

    added featured block

--- sites/all/themes/atento/ckeditor.styles.js
+++ sites/all/themes/atento/ckeditor.styles.js
@@ -8,14 +8,78 @@ For licensing, see LICENSE.html or http://ckeditor.com/license
  * The 'Styles' button is not enabled by default in DrupalFull and DrupalFiltered toolbars.
  */
 if(typeof(CKEDITOR) !== 'undefined') {
-    CKEDITOR.addStylesSet( 'drupal',
-    [
-            {
-              name: 'Color Container', //THESE NEXT 4 LINES I CHANGED 
-              element: 'div',
-              attributes: {
-                class: 'color-container'
-              }
-            }
-    ]);
+  CKEDITOR.addStylesSet( 'drupal',
+  [
+    {
+      name: 'Square Container', //THESE NEXT 4 LINES I CHANGED 
+      element: 'div',
+      attributes: {
+        class: 'color-container'
+      }
+    }, 
+    {
+      name: 'Square Container (Dark Blue)', //THESE NEXT 4 LINES I CHANGED 
+      element: 'div',
+      attributes: {
+        class: 'color-container dark-blue'
+      }
+    }, 
+    {
+      name: 'Square Container (Blue)', //THESE NEXT 4 LINES I CHANGED 
+      element: 'div',
+      attributes: {
+        class: 'color-container blue'
+      }
+    }, 
+    {
+      name: 'Square Container (Orange)', //THESE NEXT 4 LINES I CHANGED 
+      element: 'div',
+      attributes: {
+        class: 'color-container orange'
+      }
+    }, 
+    {
+      name: 'Square Container (Green)', //THESE NEXT 4 LINES I CHANGED 
+      element: 'div',
+      attributes: {
+        class: 'color-container green'
+      }
+    }, 
+    
+    {
+      name: 'Featured Container', //THESE NEXT 4 LINES I CHANGED 
+      element: 'div',
+      attributes: {
+        class: 'featured-container'
+      }
+    }, 
+    {
+      name: 'Featured Container (Dark Blue)', //THESE NEXT 4 LINES I CHANGED 
+      element: 'div',
+      attributes: {
+        class: 'featured-container dark-blue'
+      }
+    }, 
+    {
+      name: 'Featured Container (Blue)', //THESE NEXT 4 LINES I CHANGED 
+      element: 'div',
+      attributes: {
+        class: 'featured-container blue'
+      }
+    }, 
+    {
+      name: 'Featured Container (Orange)', //THESE NEXT 4 LINES I CHANGED 
+      element: 'div',
+      attributes: {
+        class: 'featured-container orange'
+      }
+    }, 
+    {
+      name: 'Featured Container (Green)', //THESE NEXT 4 LINES I CHANGED 
+      element: 'div',
+      attributes: {
+        class: 'featured-container green'
+      }
+    }
+  ]);
 }
