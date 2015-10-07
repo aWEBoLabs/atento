71c9934845065618ac0f6e194c323f7bd2b3e298
cristian100 <cristian100@gmail.com>
2015-10-07 15:06:08 -0600

    added new formats

--- sites/all/themes/atento/ckeditor.styles.js
+++ sites/all/themes/atento/ckeditor.styles.js
@@ -11,42 +11,42 @@ if(typeof(CKEDITOR) !== 'undefined') {
   CKEDITOR.addStylesSet( 'drupal',
   [
     {
-      name: 'Square Container', //THESE NEXT 4 LINES I CHANGED 
+      name: 'Square Container', 
       element: 'div',
       attributes: {
         class: 'color-container'
       }
     }, 
     {
-      name: 'Square Container (Dark Blue)', //THESE NEXT 4 LINES I CHANGED 
+      name: 'Square Container (Dark Blue)', 
       element: 'div',
       attributes: {
         class: 'color-container dark-blue'
       }
     }, 
     {
-      name: 'Square Container (Blue)', //THESE NEXT 4 LINES I CHANGED 
+      name: 'Square Container (Blue)', 
       element: 'div',
       attributes: {
         class: 'color-container blue'
       }
     }, 
     {
-      name: 'Square Container (Orange)', //THESE NEXT 4 LINES I CHANGED 
+      name: 'Square Container (Orange)', 
       element: 'div',
       attributes: {
         class: 'color-container orange'
       }
     }, 
     {
-      name: 'Square Container (Dark Orange)', //THESE NEXT 4 LINES I CHANGED 
+      name: 'Square Container (Dark Orange)', 
       element: 'div',
       attributes: {
         class: 'color-container dark-orange'
       }
     }, 
     {
-      name: 'Square Container (Green)', //THESE NEXT 4 LINES I CHANGED 
+      name: 'Square Container (Green)', 
       element: 'div',
       attributes: {
         class: 'color-container green'
@@ -54,46 +54,88 @@ if(typeof(CKEDITOR) !== 'undefined') {
     }, 
 
     {
-      name: 'Featured Container', //THESE NEXT 4 LINES I CHANGED 
+      name: 'Featured Container', 
       element: 'div',
       attributes: {
         class: 'featured-container'
       }
     }, 
     {
-      name: 'Featured Container (Dark Blue)', //THESE NEXT 4 LINES I CHANGED 
+      name: 'Featured Container (Dark Blue)', 
       element: 'div',
       attributes: {
         class: 'featured-container dark-blue'
       }
     }, 
     {
-      name: 'Featured Container (Blue)', //THESE NEXT 4 LINES I CHANGED 
+      name: 'Featured Container (Blue)', 
       element: 'div',
       attributes: {
         class: 'featured-container blue'
       }
     }, 
     {
-      name: 'Featured Container (Orange)', //THESE NEXT 4 LINES I CHANGED 
+      name: 'Featured Container (Orange)', 
       element: 'div',
       attributes: {
         class: 'featured-container orange'
       }
     }, 
     {
-      name: 'Featured Container (Dark Orange)', //THESE NEXT 4 LINES I CHANGED 
+      name: 'Featured Container (Dark Orange)', 
       element: 'div',
       attributes: {
         class: 'featured-container dark-orange'
       }
     }, 
     {
-      name: 'Featured Container (Green)', //THESE NEXT 4 LINES I CHANGED 
+      name: 'Featured Container (Green)', 
       element: 'div',
       attributes: {
         class: 'featured-container green'
       }
-    }
+    }, 
+    {
+      name: 'Two Columns (748px width, Image Left and Description Right)', 
+      element: 'div',
+      attributes: {
+        class: 'two-columns-image-left'
+      }
+    }, 
+    {
+      name: 'Two Columns (748px width, Description Left and Image Right)', 
+      element: 'div',
+      attributes: {
+        class: 'two-columns-image-right'
+      }
+    }, 
+    {
+      name: 'One Column (264px width)', 
+      element: 'div',
+      attributes: {
+        class: 'one-column'
+      }
+    }, 
+    {
+      name: 'One Column (220px width)', 
+      element: 'div',
+      attributes: {
+        class: 'one-column width-220'
+      }
+    },  
+    {
+      name: 'One Column (194px width)', 
+      element: 'div',
+      attributes: {
+        class: 'one-column width-194'
+      }
+    },  
+    {
+      name: 'One Column (168px width)', 
+      element: 'div',
+      attributes: {
+        class: 'one-column width-168'
+      }
+    },  
   ]);
 }
